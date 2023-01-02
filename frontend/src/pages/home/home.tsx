00713078e9a48241c5d1a2596181f0e4d67047c7
Jia Yi <53046105+kazuui@users.noreply.github.com>
2023-01-02 15:20:35 +0800

    main page in-progress

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -4,7 +4,49 @@ import React from 'react';
 
 function Home() {
   return (
-    <p>This is Home page</p>
+    <div className='page'>
+      <div className='panel-container'>
+        <div className='side-panel side-panel-left'>
+          <p>Side Panel</p>
+        </div>
+        <div className='main-panel'>
+          <p>Main Panel</p>
+          {/* Post Example?? */}
+          <div className='post-item'>
+            <div className='post-user-info'>
+              <div className='post-user-profile-img'>
+                {/*  */}
+              </div>
+              <div className='post-user-text'>
+                <p className='post-user-name'>qwerty123</p>
+                <p className='post-user-location'>Location</p>
+              </div>
+              <div className='post-edit'>
+                <p>icon</p>
+              </div>
+            </div>
+            <div className='post-content'>
+              <p>Post Content</p>
+            </div>
+            <div className='post-interactions'>
+              <p>Like</p>
+              <p>Comment</p>
+              <p>Share</p>
+              <p>Bookmark</p>
+            </div>
+            <div className='post-likes'>
+              <p>Likes</p>
+            </div>
+            <div className='post-comment-summary'>
+              <p>Comment summary</p>
+            </div>
+          </div>
+        </div>
+        <div className='side-panel side-panel-right'>
+          <p>Side Panel</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
